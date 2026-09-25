8cd57a1645dc4d5e0c85603b87038351691d0d5b
otienoryan786-lab <otienoryan786@gmail.com>
2026-09-25 12:35:00 +0300

    Fix off-by-one error in calculateDiscountedPrice loop
    Loop started at i = 1 instead of i = 0, causing the function to add pricePerItem one time fewer than the quantity purchased. Changed loop initialization to i = 0 so all units are counted.

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,9 +1,9 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
+    for (let i = 0; i < quantity; i++) {
+    totalPrice += pricePerItem;
+}
 
     if (quantity >= 10) {
         totalPrice *= 0.9;
